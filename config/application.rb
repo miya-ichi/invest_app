84f9d1e2c3a148481a45ade34bf5af4df18403c2
miyaichi <burnedsoba@gmail.com>
2023-02-14 00:45:22 +0900

    modify rails generateで生成されるファイルを制限

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,13 @@ module InvestApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec
+      g.view_specs false
+      g.helper_specs false
+      g.request_specs false
+      g.assets false
+      g.helper false
+    end
   end
 end
