e3446779fe8c2a0c8b6a8e9fc8afe902ac3d4487
miyaichi <burnedsoba@gmail.com>
2023-02-13 16:43:25 +0900

    add Userモデルのマイグレーション

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,3 +3,22 @@ FactoryBot.define do
     
   end
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  admin            :boolean          default(FALSE), not null
+#  crypted_password :string
+#  email            :string           not null
+#  salt             :string
+#  username         :string           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email) UNIQUE
+#  index_users_on_username  (username) UNIQUE
+#
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,3 +3,22 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  admin            :boolean          default(FALSE), not null
+#  crypted_password :string
+#  email            :string           not null
+#  salt             :string
+#  username         :string           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email) UNIQUE
+#  index_users_on_username  (username) UNIQUE
+#
