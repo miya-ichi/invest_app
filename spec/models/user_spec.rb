fb10f42d01bab4ed4c2e77541bd7cb187322bd45
miyaichi <burnedsoba@gmail.com>
2023-02-14 12:07:51 +0900

    fix Userモデルのテストのミス・・・

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,28 +4,33 @@ RSpec.describe User, type: :model do
   describe 'バリデーション' do
     it 'ユーザー名は必須であること' do
       user = build(:user, username: nil)
+      user.valid?
       expect(user.errors[:username]).to include('を入力してください')
     end
 
     it 'ユーザー名は一意であること' do
       create(:user, username: 'ユーザー')
-      user_b = build(:user, username: 'ユーザー')
-      expect(user_b.errors[:username]).to include('はすでに存在します')
+      user = build(:user, username: 'ユーザー')
+      user.valid?
+      expect(user.errors[:username]).to include('はすでに存在します')
     end
 
     it 'メールアドレスは必須であること' do
       user = build(:user, email: nil)
+      user.valid?
       expect(user.errors[:email]).to include('を入力してください')
     end
 
     it 'メールアドレスは一意であること' do
       create(:user, email: 'test@example.com')
-      user_b = build(:user, email: 'test@example.com')
-      expect(user_b.errors[:email]).to include('はすでに存在します')
+      user = build(:user, email: 'test@example.com')
+      user.valid?
+      expect(user.errors[:email]).to include('はすでに存在します')
     end
 
     it 'パスワードは6文字以上必要であること' do
       user = build(:user, password: 'a' * 5, password_confirmation: 'a' * 5)
+      user.valid?
       expect(user.errors[:password]).to include('は6文字以上で入力してください')
     end
   end
