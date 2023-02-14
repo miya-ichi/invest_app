fb423c5038792d70cd89bddb0c7844e9b232dd6b
miyaichi <burnedsoba@gmail.com>
2023-02-14 23:04:10 +0900

    add ユーザー登録機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,25 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
   end
 
   def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: 'ユーザー登録が完了しました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
