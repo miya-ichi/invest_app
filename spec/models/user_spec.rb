b77487ca41e79eaa91dcdb32d0df6e1defad5974
miyaichi <burnedsoba@gmail.com>
2023-02-14 11:27:23 +0900

    modify Userモデルのテストを修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,34 +4,29 @@ RSpec.describe User, type: :model do
   describe 'バリデーション' do
     it 'ユーザー名は必須であること' do
       user = build(:user, username: nil)
-      user.valid?
       expect(user.errors[:username]).to include('を入力してください')
     end
 
     it 'ユーザー名は一意であること' do
-      user_a = create(:user, username: 'ユーザー')
+      create(:user, username: 'ユーザー')
       user_b = build(:user, username: 'ユーザー')
-      user_b.valid?
       expect(user_b.errors[:username]).to include('はすでに存在します')
     end
 
     it 'メールアドレスは必須であること' do
       user = build(:user, email: nil)
-      user.valid?
       expect(user.errors[:email]).to include('を入力してください')
     end
 
     it 'メールアドレスは一意であること' do
-      user_a = create(:user, email: 'test@example.com')
+      create(:user, email: 'test@example.com')
       user_b = build(:user, email: 'test@example.com')
-      user_b.valid?
       expect(user_b.errors[:email]).to include('はすでに存在します')
     end
 
-    it 'パスワードは6文字以上であること' do
+    it 'パスワードは6文字以上必要であること' do
       user = build(:user, password: 'a' * 5, password_confirmation: 'a' * 5)
-      user.valid?
-      expect(user.errors[:password]).to include('6文字以上')
+      expect(user.errors[:password]).to include('は6文字以上で入力してください')
     end
   end
 end
