d0cdd0c03e325faad9b5d1b5522dd0443ab2e0c9
miyaichi <burnedsoba@gmail.com>
2023-02-15 09:45:29 +0900

    add ログイン/ログアウト機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_back_or_to login_url, notice: 'ログインしました。'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to login_url, notice: 'ログアウトしました。'
+  end
 end
