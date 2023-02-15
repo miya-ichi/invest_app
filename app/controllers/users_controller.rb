560cdacf977e8ca4d47bb455026643a2a454385f
miyaichi <burnedsoba@gmail.com>
2023-02-15 22:57:56 +0900

    fix rubocop -a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,11 @@
 class UsersController < ApplicationController
-  def show
-  end
-  
+  def show; end
+
   def new
     @user = User.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @user = User.new(user_params)
