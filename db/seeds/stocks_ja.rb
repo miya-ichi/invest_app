cb37a2ca219827cd3d6907fc3760d8d0a980de06
miyaichi <burnedsoba@gmail.com>
2023-02-18 00:18:30 +0900

    fix テストしてた時の残骸削除

--- db/seeds/stocks_ja.rb
+++ db/seeds/stocks_ja.rb
@@ -2,7 +2,6 @@ require 'csv'
 
 puts 'Start inserting seed "stocks[japan]"'
 
-i = 0
 CSV.foreach('./db/seeds/japan_stocks.csv', headers: true) do |row|
   Stock.create(category: 0, code: row['code'], name: row['name'])
 end
