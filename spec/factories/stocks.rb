8898431a84dfc5d63350f6c6eb740bc37c0769c2
miyaichi <burnedsoba@gmail.com>
2023-02-17 22:36:38 +0900

    add Stockモデルのファクトリ

--- spec/factories/stocks.rb
+++ spec/factories/stocks.rb
@@ -1,10 +1,29 @@
 FactoryBot.define do
   factory :stock do
-    category { 1 }
-    code { "MyString" }
-    name { "MyString" }
-    website { "MyString" }
-    description { "MyText" }
-    sector { "MyString" }
+    category { 0 }
+    code { '1000' }
+    name { '日本株式' }
+    website { 'https://example.com' }
+    description { '日本の上場企業の概要を登録する' }
+    sector { 'テスト' }
   end
 end
+
+# == Schema Information
+#
+# Table name: stocks
+#
+#  id          :bigint           not null, primary key
+#  category    :integer          not null
+#  code        :string           not null
+#  description :text
+#  name        :string           not null
+#  sector      :string
+#  website     :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_stocks_on_code  (code) UNIQUE
+#
