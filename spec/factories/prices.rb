608989d62eaf325e4121b08a391ec0b70419db25
miyaichi <burnedsoba@gmail.com>
2023-02-20 17:47:42 +0900

    add Priceモデルのファクトリ

--- spec/factories/prices.rb
+++ spec/factories/prices.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :price do
-    stock { nil }
-    date { "2023-02-20" }
+    stock
+    date { Time.zone.today }
     market_open { 1.5 }
     daily_high { 1.5 }
     daily_row { 1.5 }
