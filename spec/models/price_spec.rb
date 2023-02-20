485c401e47105b7e83b8f8e5f0bce1b74fe8e42d
miyaichi <burnedsoba@gmail.com>
2023-02-20 17:56:51 +0900

    add Priceモデルのバリデーションテスト

--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Price, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  decsribe 'バリデーション' do
+    it '日付は必須であること' do
+      price = build(:price, date: nil)
+      price.valid?
+      expect(price.errors[:date]).to include('を入力してください')
+    end
+
+    it '日付は一意であること' do
+      create(:price, date: Time.zone.today)
+      price = build(:price, date: Time.zone.today)
+      price.valid?
+      expect(price.errors[:date]).to include('はすでに存在します')
+    end
+
+    it '終値は必須であること' do
+      price = build(:price, market_close: nil)
+      price.valid?
+      expect(price.errors[:market_close]).to include('を入力してください')
+    end
+  end
 end
