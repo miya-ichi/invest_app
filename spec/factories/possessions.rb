7ed6f0ab74c9e61dc47949546e95cad65f172adc
miyaichi <burnedsoba@gmail.com>
2023-02-23 13:33:08 +0900

    add Possessionモデルのファクトリ

--- spec/factories/possessions.rb
+++ spec/factories/possessions.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :possession do
-    user { nil }
-    stock { nil }
-    volume { 1 }
-    price { 1.5 }
+    user
+    stock
+    volume { rand(1..10) }
+    price { rand(1..100) }
   end
 end
 
