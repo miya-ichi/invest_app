6fb3110f7e1eef7e6ea339e78e7b5124ee35cef3
miyaichi <burnedsoba@gmail.com>
2023-02-23 15:22:51 +0900

    add possessionsのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'possessions/index'
+  root to: 'possessions#index'
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
   resources :users, except: %i(index)
+  resources :possessions, except: %i(show)
 end
