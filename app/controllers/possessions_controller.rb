6a55de5d901f3f1d4221238871906e0b761c11da
miyaichi <burnedsoba@gmail.com>
2023-02-25 23:31:24 +0900

    add 保有銘柄の登録機能を実装

--- app/controllers/possessions_controller.rb
+++ app/controllers/possessions_controller.rb
@@ -1,4 +1,26 @@
 class PossessionsController < ApplicationController
   def index
+    @possessions = current_user.possessions.all.order(created_at: :desc)
+  end
+
+  def new
+    @possession = current_user.possessions.build
+  end
+
+  def create
+    @possession = current_user.possessions.build(possession_params)
+
+    if @possession.save
+      @possession.stock.set_prices
+      flash.now[:success] = "「#{@possession.stock.name}」を保有銘柄に追加しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def possession_params
+    params.require(:possession).permit(:stock_id, :price, :volume, :memo)
   end
 end
