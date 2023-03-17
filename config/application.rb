5d3aec27d1d2914778b2b255ea6e1b065adf8c53
miyaichi <burnedsoba@gmail.com>
2023-03-17 12:06:03 +0900

    modify Railsの時間設定を日本へ

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,7 @@ module InvestApp
 
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    config.time_zone = 'Asia/Tokyo'
   end
 end
