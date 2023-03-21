2e377b8d8bb1da9da9b4bb9187712037340f0dae
miyaichi <burnedsoba@gmail.com>
2023-03-21 17:55:10 +0900

    add トップページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  root to: 'possessions#index'
+  root to: 'static_pages#home'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
