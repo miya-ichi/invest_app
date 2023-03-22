cbd07d1d8c239991c71ff10589bf86fc3a84142e
miyaichi <burnedsoba@gmail.com>
2023-03-22 10:37:15 +0900

    delete users_controllerの不要なアクションを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
-  def show; end
-
   def new
     @user = User.new
   end
-
-  def edit; end
 
   def create
     @user = User.new(user_params)
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  resources :users, except: %i(index)
+  resources :users, except: %i(index show edit)
   resources :possessions, except: %i(show)
 end
