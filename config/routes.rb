046fd8314187f9bbd77a03f2299a817d5f3fd379
miyaichi <burnedsoba@gmail.com>
2023-04-09 00:46:38 +0900

    add dashboardへのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
   root to: 'static_pages#home'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  get 'dashboard', to: 'dashboard#index'
 
   resources :users, except: %i(index show edit)
   resources :possessions, except: %i(show)
