bf27117d9a9bf10a5bb6af26790f0f8035963691
miyaichi <burnedsoba@gmail.com>
2023-04-22 00:05:40 +0900

    add NoteのCRまで実装

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,39 @@
 class NotesController < ApplicationController
   def index
+    @notes = current_user.notes
+  end
+
+  def show
+    @note = current_user.notes.find(params[:id])
   end
 
   def new
+    @note = current_user.notes.build
   end
 
   def edit
+    @note = current_user.notes.find(params[:id])
+
+    if @note.update(note_params)
+      redirect_to note_url(@note), success: "ノート「#{@note.title}」を更新しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @note = current_user.notes.build(note_params)
+
+    if @note.save
+      redirect_to note_url(@note), success: "ノート「#{@note.title}」を作成しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title)
   end
 end
