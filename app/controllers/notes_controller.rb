22aeb15f36f4ad580a7d1a59e7387a9364f00e17
miyaichi <burnedsoba@gmail.com>
2023-04-23 23:32:24 +0900

    add NoteのUDを実装

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,22 +13,33 @@ class NotesController < ApplicationController
 
   def edit
     @note = current_user.notes.find(params[:id])
-
-    if @note.update(note_params)
-      redirect_to note_url(@note), success: "ノート「#{@note.title}」を更新しました。"
-    else
-      render :new, status: :unprocessable_entity
-    end
   end
-
+  
   def create
     @note = current_user.notes.build(note_params)
-
+    
     if @note.save
       redirect_to note_url(@note), success: "ノート「#{@note.title}」を作成しました。"
     else
       render :new, status: :unprocessable_entity
     end
+  end
+  
+  def update
+    @note = current_user.notes.find(params[:id])
+    
+    if @note.update(note_params)
+      flash.now[:success] = "ノート「#{@note.title}」を更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @note = current_user.notes.find(params[:id])
+
+    @note.destroy!
+    redirect_to notes_url, success: "ノート「#{@note.title}」を削除しました。", status: :see_other
   end
 
   private
