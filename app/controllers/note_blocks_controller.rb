5a23b3afa422d9003a1180833a68221710e6f218
miyaichi <burnedsoba@gmail.com>
2023-04-27 00:03:03 +0900

    add ノートの明細のcreateを実装

--- app/controllers/note_blocks_controller.rb
+++ app/controllers/note_blocks_controller.rb
@@ -1,2 +1,11 @@
 class NoteBlocksController < ApplicationController
+  def create
+    @note = current_user.notes.find(params[:note_id])
+    @note_block = @note.note_blocks.create(note_block_params)
+    redirect_to @note
+  end
+
+  def note_block_params
+    params.require(:note_block).permit(:content, :index)
+  end
 end
