09ac15dd6eff4a6e79df6dc3e053d951cd1d791f
miyaichi <burnedsoba@gmail.com>
2023-06-22 23:35:45 +0900

    add ユーザー情報の編集機能を実装

--- app/controllers/mypage/accounts_controller.rb
+++ app/controllers/mypage/accounts_controller.rb
@@ -4,5 +4,23 @@ class Mypage::AccountsController < Mypage::BaseController
   end
 
   def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+
+    if @user.update(account_params)
+      redirect_to mypage_account_url, success: 'ユーザー情報を更新しました。'
+    else
+      flash.now[:danger] = 'ユーザー情報の更新に失敗しました。'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def account_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
