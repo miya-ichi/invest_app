54823df03abecda2da469954445e23a4097ab7f1
miyaichi <burnedsoba@gmail.com>
2023-08-06 23:00:53 +0900

    add Tagモデルのバリデーションのテスト

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    it 'タグ名は必須であること' do
+      tag = build(:tag, name: nil)
+      tag.valid?
+      expect(tag.errors[:name]).to include('を入力してください')
+    end
+
+    it 'タグ名は一意であること' do
+      create(:tag, name: 'tag')
+      tag = build(:tag, name: 'tag')
+      tag.valid?
+      expect(tag.errors[:name]).to include('はすでに存在します')
+    end
+
+    it 'タグ名は15文字以内であること' do
+      tag = build(:tag, name: 'a' * 15)
+      tag.valid?
+      expect(tag.errors[:name]).not_to include('は15文字以内で入力してください')
+
+      tag = build(:tag, name: 'a' * 16)
+      tag.valid?
+      expect(tag.errors[:name]).to include('は15文字以内で入力してください')
+    end
+  end
 end
 
 # == Schema Information
