2a79b7c4c570a7799e0ec10e5417eb5cec993c08
miyaichi <burnedsoba@gmail.com>
2023-08-06 23:32:48 +0900

    rails g model notes_tag

---
+++ app/models/notes_tag.rb
@@ -0,0 +1,4 @@
+class NotesTag < ApplicationRecord
+  belongs_to :tag
+  belongs_to :note
+end
---
+++ spec/factories/notes_tags.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :notes_tag do
+    tag { nil }
+    note { nil }
+  end
+end
