edbd76972e32c1a3f89676bdd0e024536f666497
miyaichi <burnedsoba@gmail.com>
2023-08-09 22:41:08 +0900

    add ノートのタグ機能を実装

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,22 +13,27 @@ class NotesController < ApplicationController
 
   def edit
     @note = current_user.notes.find(params[:id])
+    @tags = @note.tags.pluck(:name)
   end
 
   def create
     @note = current_user.notes.build(note_params)
-
+    tags = params[:note][:tags].split(',')
+    
     if @note.save
+      @note.save_notes_tags(tags)
       redirect_to note_url(@note), success: "ノート「#{@note.title}」を作成しました。"
     else
       render :new, status: :unprocessable_entity
     end
   end
-
+  
   def update
     @note = current_user.notes.find(params[:id])
-
+    tags = params[:note][:tags].split(',')
+    
     if @note.update(note_params)
+      @note.save_notes_tags(tags)
       flash.now[:success] = "ノート「#{@note.title}」を更新しました。"
     else
       render :edit, status: :unprocessable_entity
--- app/models/note.rb
+++ app/models/note.rb
@@ -6,6 +6,21 @@ class Note < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :private, presence: true
+
+  def save_notes_tags(tags)
+    current_tags = self.tags.pluck(:name)
+    old_tags = current_tags - tags
+    new_tags = tags - current_tags
+
+    old_tags.each do |old_name|
+      self.tags.delete(Tag.find_by(name: old_name))
+    end
+
+    new_tags.each do |new_name|
+      notes_tag = Tag.find_or_create_by(name: new_name)
+      self.tags << notes_tag
+    end
+  end
 end
 
 # == Schema Information
